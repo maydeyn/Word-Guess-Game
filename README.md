1c0e12e1c65ddf0a4de9e598b9de8dcdea06ad70
May Zhou <maydeyn@gmail.com>
2018-11-09 01:29:51 -0600

    pseudo-code and base content update

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # Word-Guess-Game
 
-# Pesudo-code
+# Pesudo-code (JavaScript)
+1. create a list of words
+2. create a list of hints corresponding to the words
+3. randomly picks words
+4. create underscores based on length of word
+5. get user input 
+    - user can use both keypress AND clickable
+    - instead of listing out the used letters, make used letter unclickable (with visual)
+6. convert user input to all caps
+7. check if user guess is correct
+8. push 
 
-1. include 
+
+# Research
+- object
+- event listener
+- audio
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,10 @@
-$(document).ready(function(){
+// list of words
+var words = ['overwatch','diablo','fortnite','world of warcraft', 'hearthstone', 'league of legends', 'dota', 'dying light', 'super mario', 'final fantasy', 'portal', 'street fighter', 'mortal combat', 'doom', 'legend of zelda', 'monster hunter', 'destiny', 'call of duty', 'pokemon']
+// randomize choice of words
+var randomNum = Math.floor(Math.random() * words.length);
+var chooseWord = words[randomNum];
+// underscore base on length of word
+var 
+for (var i = 0; i < chooseWord.length; i++) {
 
-    
-})
+}
